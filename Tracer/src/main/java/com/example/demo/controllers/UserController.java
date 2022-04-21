abd19d26c68366782a147cf73a03c76422a1eb5a
Philip Wentworth <guitartheist@gmail.com>
2022-04-21 02:59:23 -0400

    added /user/{username} to retrieve user profile info

--- Tracer/src/main/java/com/example/demo/controllers/UserController.java
+++ Tracer/src/main/java/com/example/demo/controllers/UserController.java
@@ -1,7 +1,5 @@
 package com.example.demo.controllers;
 
-import java.util.Locale;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -12,6 +10,8 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,6 +43,12 @@ public class UserController {
 		this.jwtTokenUtil = jwtTokenUtil;
 		this.authService = authService;
 		this.passwordEncoder = passwordEncoder;
+	}
+	
+	@GetMapping("/{username}")
+	@ResponseBody
+	public AppUser getUser(@PathVariable("username") String username) {
+		return userService.findUserByUsername(username);
 	}
 	
 	@PostMapping("/register")
